b5665a1318ba089a9841bdd557ca7b36eef0ca14
joudar <joud_alrumayh@outlook.com>
2024-05-30 13:42:03 +0300

    HW

---
+++ Day2/StudentManager.java
@@ -0,0 +1,38 @@
+package Day2;
+
+import java.util.ArrayList;
+
+public class StudentManager {
+    private ArrayList<Student> students;
+
+    public StudentManager() {
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public Student getStudentById(int id) {
+        for (Student student : students) {
+            if (student.getId() == id) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Student> getAllStudents() {
+        return students;
+    }
+
+    public boolean removeStudentById(int id) {
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getId() == id) {
+                students.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+}
